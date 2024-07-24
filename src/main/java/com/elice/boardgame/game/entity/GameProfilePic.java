2c1772b864096847f6ce7a0877ef2cf9bbe6b023
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-07-24 17:02:55 +0900

    FEAT: 게임 엔티티 작성중

---
+++ src/main/java/com/elice/boardgame/game/entity/GameProfilePic.java
@@ -0,0 +1,25 @@
+package com.elice.boardgame.game.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameProfilePic extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long picId;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private BoardGame boardGame;
+
+    private String picAddress;
+}
