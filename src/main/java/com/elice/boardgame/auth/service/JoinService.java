fb93aa92d0d19974e986d438ff8202a33d9ab27a
[Cloud3] 민경호 <mm990224@naver.com>
2024-07-26 10:15:19 +0900

    feat: 자체회원가입 및 jwt토큰 생성 기능 추가

---
+++ src/main/java/com/elice/boardgame/auth/service/JoinService.java
@@ -0,0 +1,38 @@
+package com.elice.boardgame.auth.service;
+
+import com.elice.boardgame.auth.dto.JoinDTO;
+import com.elice.boardgame.auth.entity.User;
+import com.elice.boardgame.auth.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+            return;
+        }
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        user.setRole("ROLE_ADMIN");
+
+        userRepository.save(user);
+    }
+}
