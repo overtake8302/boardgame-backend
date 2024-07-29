c0b20a82ab1f3eb13ccfee26ac8202a4e8d90889
[Cloud3] 신준영 <2261-jy.shin@users.noreply.kdt-gitlab.elice.io>
2024-07-29 22:16:46 +0900

    Rename QueryDslConfig

--- src/main/java/com/elice/boardgame/config/QuerydslConfig.java
+++ src/main/java/com/elice/boardgame/config/QuerydslConfig.java
@@ -1,14 +1,13 @@
 package com.elice.boardgame.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
 @Configuration
-public class QuerydslConfig {
+public class QueryDslConfig {
 
     @PersistenceContext
     private EntityManager entityManager;
