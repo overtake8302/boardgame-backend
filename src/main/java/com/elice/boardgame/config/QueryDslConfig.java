a0665d41b5ca6939223bc1758fcf05dec3fcb99d
[Cloud3] 우문식 <woomoon1107@gmail.com>
2024-07-30 15:33:36 +0900

    Update: QueryDslConfig.java

--- src/main/java/com/elice/boardgame/config/QueryDslConfig.java
+++ src/main/java/com/elice/boardgame/config/QueryDslConfig.java
@@ -1,10 +1,11 @@
 package com.elice.boardgame.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Configuration
 public class QueryDslConfig {
