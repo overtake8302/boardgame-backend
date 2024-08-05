7148d3409385226605fb3c2ae746648216ad4059
[Cloud3] 우문식 <woomoon1107@gmail.com>
2024-08-05 17:31:38 +0900

    Merge: feat/category

--- src/main/java/com/elice/boardgame/report/controller/ReportController.java
+++ src/main/java/com/elice/boardgame/report/controller/ReportController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/report")
 public class ReportController {
 
     private final ReportService reportService;
@@ -19,12 +21,12 @@ public class ReportController {
         return reportService.findAll();
     }
 
-    @PutMapping()
+    @PutMapping("/send")
     public void sendReport() {
 
     }
 
-    @PutMapping()
+    @PutMapping("/update")
     public void updateReport() {
 
     }
