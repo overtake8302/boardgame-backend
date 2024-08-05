b324c2c13095be462f10e3ee8f6dec223b3fb8bd
현태 김 <603080@daum.net>
2024-08-05 09:22:29 +0900

    feat: Get Friends 메서드 수정

--- src/main/java/com/elice/boardgame/social/controller/SocialController.java
+++ src/main/java/com/elice/boardgame/social/controller/SocialController.java
@@ -1,6 +1,7 @@
 package com.elice.boardgame.social.controller;
 
 import com.elice.boardgame.common.dto.CommonResponse;
+import com.elice.boardgame.common.dto.PaginationRequest;
 import com.elice.boardgame.social.dto.SocialRequest;
 import com.elice.boardgame.social.service.SocialService;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +27,10 @@ public class SocialController {
     private final SocialService socialService;
 
     @GetMapping
-    public ResponseEntity<CommonResponse<List<Long>>> getFriends(@RequestParam int page, @RequestParam int size) {
+    public ResponseEntity<CommonResponse<List<Long>>> getFriends(@ModelAttribute PaginationRequest paginationRequest) {
         Long userId = 1L; // 로그인된 계정 아이디 받아오는 메서드로 변경
+        int page = paginationRequest.getPage();
+        int size = paginationRequest.getSize();
         List<Long> friends = socialService.getFriendIds(userId, page, size);
         CommonResponse<List<Long>> response = CommonResponse.<List<Long>>builder()
             .payload(friends)
