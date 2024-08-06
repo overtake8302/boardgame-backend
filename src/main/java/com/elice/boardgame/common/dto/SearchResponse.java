af195bb20a488ec4abeac9d0586b2ae639380c04
현태 김 <603080@daum.net>
2024-08-06 21:38:11 +0900

    feat: SocialController API 수정, common/dto/SearchResponse 생성

---
+++ src/main/java/com/elice/boardgame/common/dto/SearchResponse.java
@@ -0,0 +1,11 @@
+package com.elice.boardgame.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchResponse {
+    private Long id;
+    private String name;
+}
