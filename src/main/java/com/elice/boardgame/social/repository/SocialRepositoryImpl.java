cdd54938bfd1715df1f46308847b30a23e585d6b
현태 김 <603080@daum.net>
2024-08-09 10:13:33 +0900

    feat: Show friend name, pagination

--- src/main/java/com/elice/boardgame/social/repository/SocialRepositoryImpl.java
+++ src/main/java/com/elice/boardgame/social/repository/SocialRepositoryImpl.java
@@ -1,11 +1,8 @@
 package com.elice.boardgame.social.repository;
 
 import static com.elice.boardgame.social.entity.QSocial.social;
-import com.elice.boardgame.social.entity.Social;
 import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
@@ -19,11 +16,10 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class SocialRepositoryImpl implements SocialRepositoryCustom {
     private final JPAQueryFactory queryFactory;
-    private final EntityManager entityManager;
 
     @Override
-    public Page<Long> findFriendIdsByUserId(Long userId, Pageable pageable) {
-        List<Long> friendIds = queryFactory
+    public List<Long> findFriendIdsByUserId(Long userId) {
+        return queryFactory
             .select(
                 new CaseBuilder()
                     .when(social.id.userId.eq(userId)).then(social.id.friendId)
@@ -31,16 +27,7 @@ public class SocialRepositoryImpl implements SocialRepositoryCustom {
             )
             .from(social)
             .where(social.id.userId.eq(userId).or(social.id.friendId.eq(userId)))
-            .offset(pageable.getOffset())
-            .limit(pageable.getPageSize())
             .fetch();
-
-        long total = queryFactory
-            .selectFrom(social)
-            .where(social.id.userId.eq(userId).or(social.id.friendId.eq(userId)))
-            .fetchCount();
-
-        return new PageImpl<>(friendIds, pageable, total);
     }
 
     @Override
@@ -59,12 +46,5 @@ public class SocialRepositoryImpl implements SocialRepositoryCustom {
             .where((social.id.userId.eq(userId).and(social.id.friendId.eq(friendId)))
                 .or(social.id.userId.eq(friendId).and(social.id.friendId.eq(userId))))
             .execute();
-    }
-
-    @Override
-    @Transactional
-    public Social save(Social social) {
-        entityManager.persist(social);
-        return social;
     }
 }
